c30c8caeee46e2be679e11d1141685015eb4ca99
Vyn <157186731+Vyvernl@users.noreply.github.com>
2024-06-28 17:49:36 +0530

    Add files via upload

--- SolaraFix.bat
+++ SolaraFix.bat
@@ -1,10 +1,14 @@
 @echo off
 title Solara Bootstrapper Fix
+
+REM Define the folder path dynamically using the USERPROFILE environment variable
+set "solara_dir=%USERPROFILE%\AppData\Local\Temp\Solara.Dir"
+
 REM Delete the specified folder and its contents
-rmdir /S /Q "C:\Users\vyver\AppData\Local\Temp\Solara.Dir"
+rmdir /S /Q "%solara_dir%"
 
 REM Check if the deletion was successful
-if exist "C:\Users\vyver\AppData\Local\Temp\Solara.Dir" (
+if exist "%solara_dir%" (
     echo Solara isn't installed yet.
     pause
     exit /b 1
@@ -12,9 +16,19 @@ if exist "C:\Users\vyver\AppData\Local\Temp\Solara.Dir" (
     echo Fixed bootstrapper.
 )
 
-REM Run the SolaraBootstrapper file from the same directory as this script
+REM Run the SolaraBootstrapper.exe file from the same directory as this script
 set "SCRIPT_DIR=%~dp0"
-start "" "%SCRIPT_DIR%SolaraBootstrapper"
+start "" "%SCRIPT_DIR%SolaraBootstrapper.exe"
+
+REM Add the folder exclusion to Windows Defender
+powershell -Command "Add-MpPreference -ExclusionPath '%solara_dir%'"
+
+REM Check if the exclusion was added successfully
+if %ERRORLEVEL% EQU 0 (
+    echo Solara was excluded from Windows Defender.
+) else (
+    echo Failed to exclude Solara please run as administrator.
+)
 
 REM Optionally, pause to keep the command prompt window open
 pause
